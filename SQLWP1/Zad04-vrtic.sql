b031a5b42e8390f96fd292843408b8541e858be6
Antonio Subaric <131702166+subaric98@users.noreply.github.com>
2023-05-21 21:32:29 +0200

    insert naredba bez altera

--- SQLWP1/Zad04-vrtic.sql
+++ SQLWP1/Zad04-vrtic.sql
@@ -25,3 +25,25 @@ create table dijeca (
 	skupina int not null,
 	dob int,
 );
+
+
+---DODATI ALTER
+
+select * from skupina;
+insert into skupina(brojdjece,soba,odgajateljica)
+values ('11',1,2),
+		('16',3,1),
+		('14',2,3);
+
+
+select * from odgajateljica;
+insert into odgajateljica(ime,prezime)
+values ('Korina','Horvat'),
+		('Luka','Horvat'),
+		('Marijana','Horvat');
+
+select * from dijeca;
+insert into dijeca(ime,prezime,skupina,dob)
+values ('dijete01','dijeca01', 1, 5),
+		('dijete02','dijeca02',2,4),
+		('dijete03','dijeca03',3,3);
